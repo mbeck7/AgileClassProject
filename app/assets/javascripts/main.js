3f5761a79d511d33b7cb3990076eb186282d37cc
Marc Beck <mwbeck29@gmail.com>
2014-11-16 05:02:06 +0000

    Added form validations to main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,7 +4,49 @@ $(document).ready(function () {
       jobTitle: {
         required: true
       },
+      company: {
+        required: true
+      },
       jobAddress: {
+        required: true
+      },
+      city: {
+        required: true
+      },
+      state: {
+        required: true
+      },
+      zip: {
+        required: true,
+        number: true
+      },
+      salary: {
+        required: true,
+        number: true
+      },
+      hours: {
+        required: true,
+        number: true
+      },
+      education: {
+        required: true
+      },
+      jobDescription: {
+        required: true
+      },
+      insurance: {
+        required: true
+      },
+      leaveHoliday: {
+        required: true
+      },
+      educationTraining: {
+        required: true
+      },
+      otherBenefits: {
+        required: true
+      },
+      companyProfile: {
         required: true
       },
     }
