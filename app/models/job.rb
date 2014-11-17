9774f3422c7b5231178bccbb25ed2fdd73d1acdc
Marc Beck <mwbeck29@gmail.com>
2014-11-17 05:31:35 +0000

    Added proper validation to job.rb

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,10 @@
 class Job < ActiveRecord::Base
+  validates_presence_of :company_name
+  validates_presence_of :title
+  validates_presence_of :job_type
+  validates_presence_of :job_description
+  validates_presence_of :hours_per_week
+  validates_presence_of :pay_min
+  validates_presence_of :applicant_experience
+  validates_presence_of :how_to_apply
 end
