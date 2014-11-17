f67321f13260601e0e2915494a9472516a353489
TopShelfBullard <amb@amitchellbullard.com>
2014-11-16 22:44:01 -0600

    Sets up the index, new, and create actions of the jobs controller.

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -1,10 +1,36 @@
 class JobController < ApplicationController
   def index
+    @jobs = Job.first(20)
   end
 
   def show
   end
 
   def new
+    @job = Job.new
   end
+
+  def create
+    @job = Job.new(job_params)
+
+    respond_to do |format|
+      if @job.save
+        format.html { redirect_to @job, notice: 'Job was successfully created.' }
+        format.json { render :show, status: :created, location: @job }
+      else
+        format.html { render :new }
+        format.json { render json: @job.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def set_job
+      @job = Job.find(params[:id])
+    end
+
+    def job_params
+      params.require(:job).permit(:company_name, :title, :job_type, :job_description, :hours_per_week, :pay_min, :pay_max, :pay_comment, :applicant_experience, :how_to_apply)
+    end
+
 end
