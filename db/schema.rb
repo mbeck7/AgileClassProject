528a60887cc87f95c3f089be1a5defd0c07eedd4
TopShelfBullard <amb@amitchellbullard.com>
2014-11-27 10:08:30 -0600

    Adds company rep to the database schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124161913) do
+ActiveRecord::Schema.define(version: 20141127152904) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,24 @@ ActiveRecord::Schema.define(version: 20141124161913) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "company_reps", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "company_reps", ["email"], name: "index_company_reps_on_email", unique: true
+  add_index "company_reps", ["reset_password_token"], name: "index_company_reps_on_reset_password_token", unique: true
 
   create_table "jobs", force: true do |t|
     t.string   "company_name"
