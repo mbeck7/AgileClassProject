deb635582e34e687748e09362bc70b578c399280
sschlinsog <Samuel.Schlinsog@gmail.com>
2014-12-06 01:05:03 -0600

    Adds two methods to the homeController. The first is a search method whitch is there so you know there is a search method happening. The second search_results method is taking the business logic and routing the information from the view pages to the model.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,4 +9,15 @@ class HomeController < ApplicationController
     @job = Job.find(params[:id])
     @company = Company.find(@job.company_id)
   end
+
+  def search #Displays the search bar at home#index
+  end
+ 
+  def search_results #Displays search results
+    title = params[:search_title]
+    status = params[:job_status]
+    location = params[:search_location]
+    type = params[:job_type]
+    @found_jobs = Job.title_search(title, status, location, type)
+  end
 end
