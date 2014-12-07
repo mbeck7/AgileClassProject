22b11a9532fe5dfe44e1f1e7585621000cda8b66
TopShelfBullard <amb@amitchellbullard.com>
2014-12-07 15:12:41 -0600

    Adds methods in the job and company models that perform common tasks.

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,12 +2,16 @@ class Company < ActiveRecord::Base
   belongs_to :company_rep
   has_many :jobs
 
-  class << self
-  
-    def approved_job_postings
-      Self.find_by(approved: true, rejected: false)
-      Self.order "created_at desc"
+    def self.only_approved(companies = Company.all)
+      companies.map{|company| Company.where(id: company, approved: true)}.flatten
+    end
+    
+    def self.only_unapproved(companies = Company.all)
+      companies.map{|company| Company.where(id: company, approved: false)}.flatten
     end
 
-  end
+    def self.ordered_companies
+      Company.order "created_at desc"
+    end
+
 end
